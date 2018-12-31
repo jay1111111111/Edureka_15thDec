eb05af1c6a981db36a859c977a0b40a66dd78495
tabhresh <tabhresh@gmail.com>
2018-12-31 12:09:41 +0530

    31stDec

--- src/module3/ValidateElement.java
+++ src/module3/ValidateElement.java
@@ -1,5 +1,7 @@
 package module3;
 
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -7,14 +9,17 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class ValidateElement {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		
 		System.setProperty("webdriver.chrome.driver", "E:\\Abhresh\\Installation_stuff\\ExeFiles\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
 
 		driver.get("http://seleniumautomationpractice.blogspot.com/2017/10/functionisogramigoogleanalyticsobjectri.html");
 
+		driver.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
+		
 		driver.findElement(By.id("profession-0")).click();
+		Thread.sleep(5000);
 		
 		WebElement element = driver.findElement(By.id("profession-0"));
 		
